b895999069566495a393241df48b4c4a9c6f488a
CuriousR82 <rosajeon82@gmail.com>
2023-09-12 01:05:43 -0400

    avatar upload w/ auth

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const User = require('../models/user');
+const multer = require('multer');
+const sharp = require('sharp');
+const auth = require('../middleware/auth')
 
 // original router
 const router = new express.Router();
+
+// helpers
+const upload = multer({
+    limits: {
+        fileSize: 100000000
+    }
+})
 
 // create new user
 router.post('/users', async (req, res) => {
@@ -43,4 +53,54 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-module.exports = router
+// delete user
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(400).send()
+        }
+
+        res.send()
+    }
+    catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+// fetch a single user
+router.get('/users/:id', async (req, res) => {
+    try {
+        const _id = req.params.id
+
+        const user = await User.findById(_id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }
+    catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    if (req.user.avatar != null) {
+        req.user.avatar = null
+    }
+    req.user.avatar = buffer
+    req.user.avatarExists = true
+    // await req.user.save()
+    // console.log(req.user.avatar)
+    res.send(buffer)
+
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+module.exports = router 
